864aea2108f4e1deb7f1cd8e6408990c1b20138c
Xinyu Yang <yangxinyu75@live.com>
2022-06-14 11:15:57 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 # Install ripgrep
 inst_rg_root(){
-    echo "Installing ripgrep..."
+    print_info "Installing ripgrep..."
     sudo apt install ripgrep
-} 
+}
 
 # Install ctags
 inst_ctags_root(){
-    echo "Installing ctags..."
+    print_info "Installing ctags..."
     sudo apt install \
         gcc make \
         pkg-config autoconf automake \
@@ -29,7 +29,7 @@ inst_ctags_root(){
 }
 
 inst_ctags_noroot(){
-    echo "Installing ctags..."
+    print_info "Installing ctags..."
     git clone https://github.com/universal-ctags/ctags.git
     cd ctags
     ./autogen.sh
@@ -43,14 +43,14 @@ inst_ctags_noroot(){
 
 # Install lf
 inst_lf_root(){
-    echo "Installing lf..."
+    print_info "Installing lf..."
     url=$(curl -Ls -w %{url_effective} -o /dev/null https://github.com/gokcehan/lf/releases/latest)
     wget ${url/tag/download}/lf-linux-amd64.tar.gz -O lf.tar.gz -nv > /dev/null
     if $(file lf.tar.gz | grep gzip > /dev/null);
     then
         mkdir lfexport
         tar zxf lf.tar.gz -C lfexport
-        if [$IS_SUDOER -eq 1];
+        if [ $IS_SUDOER -eq 1 ];
         then
             sudo mv ./lfexport/lf /usr/local/bin/
         else
@@ -68,29 +68,35 @@ inst_lf_noroot(){
 
 # Install node.js
 inst_node_root(){
-    echo "Installing nodejs..."
+    print_info "Installing nodejs..."
     curl -sL install-node.vercel.app/lts | sudo bash -s -- --prefix=/usr/local
 }
 
 inst_node_noroot(){
-    echo "Installing nodejs..."
+    print_info "Installing nodejs..."
     curl -sL install-node.vercel.app/lts | bash -s -- --prefix=$(echo ~)/.local
+}
+
+print_info(){
+    echo "==========================================="
+    echo >>>>>>>>>>>>> $1
+    echo "==========================================="
 }
 
 # Test whether this user has sudo priviledge
 if sudo -v > /dev/null;
 then
-    echo "Install as root"
+    print_info "Install as root"
     IS_SUDOER=1
     POSTFIX=root
     if ! command -v curl > /dev/null;
     then
-        echo "Installing curl"
+        print_info "Installing curl"
         sudo apt install curl
     fi
     array=(ctags lf rg node)
 else
-    echo "Install as non-root"
+    print_info "Install as non-root"
     IS_SUDOER=0
     POSTFIX=noroot
     if ! command -v curl > /dev/null;
@@ -108,7 +114,7 @@ for app in ${array[*]}
 do
     if command -v ${app} > /dev/null 2>&1;
     then
-        echo "${app} exists"
+        print_info "${app} exists"
     else
         inst_${app}_${POSTFIX}
     fi
