d96deb6d58743f96823e3d74559272adcaea6c6e
yang@TACC <yangxinyu75@live.com>
2023-06-06 09:21:22 +0000

    update ripgrep install

--- setup.sh
+++ setup.sh
@@ -101,7 +101,10 @@ inst_vim_noroot() {
 ###################################
 inst_rg_root(){
     print_info "Installing ripgrep..."
-    sudo apt install -y ripgrep
+    #sudo apt install -y ripgrep
+	curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+	sudo dpkg -i ripgrep_13.0.0_amd64.deb
+	rm ripgrep_13.0.0_amd64.deb
 }
 
 
